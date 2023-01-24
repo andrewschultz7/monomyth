3a4782bb18c5eba6b1db058127dbe93e9e4f5e40
SeanMyrom <seanmyrom@gmail.com>
2023-01-23 16:58:02 -0800

    deploy test

--- users/tests/test_02.py
+++ users/tests/test_02.py
@@ -1,15 +1,15 @@
-# from fastapi.testclient import TestClient
-# from main import app
+from fastapi.testclient import TestClient
+from main import app
 
 
-# client = TestClient(app)
+client = TestClient(app)
 
 
-# def test_create_user():
-#     data = {
-#         "email": "no@no.com",
-#         "password": "password",
-#         "role": "participant",
-#     }
-#     response = client.post("/signup", json=data)
-#     assert response.status_code == 200
+def test_create_user():
+    data = {
+        "email": "no@no.com",
+        "password": "password",
+        "role": "participant",
+    }
+    response = client.post("/signup", json=data)
+    assert response.status_code == 200
