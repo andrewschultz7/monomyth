1b871b5e30637a3b42e823cb1fdbcce2c01acb99
Patrick <patmordaunt@gmail.com>
2023-01-25 12:14:39 -0800

    Test 3 still having issues

--- campaigns/tests/test_03.py
+++ campaigns/tests/test_03.py
@@ -1,37 +1,38 @@
-# # get participant by id
+# get participant by id
 
-# from fastapi.testclient import TestClient
-# from main import app
-# from routers.participants import ParticipantOut
-# from authenticator import authenticator
-# from queries.participants import ParticipantRepository
-# from pydantic import BaseModel
+from fastapi.testclient import TestClient
+from main import app
+from routers.participants import ParticipantOut
+from authenticator import authenticator
+from queries.participants import ParticipantRepository
+from pydantic import BaseModel
 
-# class ParticipantOut(BaseModel):
-#     participant_id=3,
-#     user_id=1,
-#     character="test",
-#     event_id=1,
-#     campaign_id=1,
-
-
-# participant_out = ParticipantOut
-
-# client = TestClient(app)
+participant_out = ParticipantOut(
+    participant_id=3,
+    user_id=1,
+    character="test",
+    event_id=1,
+    campaign_id=1,
+)
 
 
-# class FakeParticipantRepository:
-#     def get_one(self, user_id):
-#         return participant_out
+client = TestClient(app)
 
-# def fake_authenticator():
-#     pass
 
-# def test_get_participant():
-#     app.dependency_overrides[
-#         authenticator.get_current_account_data
-#     ] = fake_authenticator
-#     app.dependency_overrides[ParticipantRepository] = FakeParticipantRepository
-#     response = client.get("camapaigns/1/events/1/participants/3")
-#     assert response.status_code == 200
-#     assert response.json() == participant_out
+class FakeParticipantRepository:
+    def get_one(self, user_id):
+        return participant_out
+
+
+def fake_authenticator():
+    pass
+
+
+def test_get_participant():
+    app.dependency_overrides[
+        authenticator.get_current_account_data
+    ] = fake_authenticator
+    app.dependency_overrides[ParticipantRepository] = FakeParticipantRepository
+    response = client.get("/campaigns/1/events/1/participants/3")
+    assert response.status_code == 200
+    assert response.json() == participant_out
