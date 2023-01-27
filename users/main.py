e97e7020c900303f2d73bc3148234042dbdd6fd8
SeanMyrom <seanmyrom@gmail.com>
2023-01-27 10:05:25 -0800

    CORS fix

--- users/main.py
+++ users/main.py
@@ -8,10 +8,6 @@ app = FastAPI()
 app.include_router(authenticator.router)
 app.include_router(users.router)
 
-origins = [
-    "http://localhost:3000",
-    "https://monomyth1.gitlab.io/monomyth",
-]
 
 app.add_middleware(
     CORSMiddleware,
